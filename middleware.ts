f2103a0d6c6aaf2dda74a57b880ff0c0283cd708
Attoh Ifeanyi <alexander.attoh22@gmail.com>
2026-02-04 11:24:32 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,6 @@ export function middleware(request: NextRequest) {
     const allCookies = request.cookies.getAll();
     const token = request.cookies.get("access_token")?.value;
 
-    // This log is our source of truth. Check your Railway logs for this!
-    console.log("--- Middleware Check ---");
-    console.log("Path:", request.nextUrl.pathname);
-    console.log("All Cookies available:", allCookies.map(c => c.name));
-    console.log("Token present:", !!token);
-
     const isNotesRoute = request.nextUrl.pathname.startsWith("/notes");
 
     if (isNotesRoute && !token) {
